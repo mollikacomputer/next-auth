3f0c9105cd13fdb676ab4ede3944bb4dde921957
MollikaComputer Provider <mollikacomputer3@gmail.com>
2023-04-23 20:01:28 +0600

    app file modify for auth

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,12 @@ import Navbar from '@/components/Navbar'
 import '@/styles/globals.css'
 import { SessionProvider } from "next-auth/react"
 
-export default function App({ Component, pageProps }) {
+export default function App({ 
+  Component, 
+  pageProps: { session, ...pageProps } }) {
   return (
   <>
-  <SessionProvider>  
+  <SessionProvider session={session} >  
   <Navbar/>
   <Component {...pageProps} />
   </SessionProvider>
