8ef2441f24f470f7bc4e42cfd935b1666d005beb
MollikaComputer Provider <mollikacomputer3@gmail.com>
2023-04-23 08:55:30 +0600

    title meta  update

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,13 @@
 const about = () => {
-    return (
-        <div className="h-screen flex items-center justify-center">
-            <h2 className="text-3xl  ">About Page</h2>
-        </div>
-    );
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <title>About Page || Ranjit k</title>
+      <meta name="description" content="About page by create next app" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" href="/favicon.ico" />
+      <h2 className="text-3xl  ">About Page</h2>
+    </div>
+  );
 };
 
 export default about;
--- pages/contact.js
+++ pages/contact.js
@@ -1,9 +1,13 @@
 const contact = () => {
-    return (
-        <div className="h-screen flex items-center justify-center">
-            <h2 className="text-3xl  ">Contact Page</h2>
-        </div>
-    );
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <title> Contact Page || Ranjit </title>
+      <meta name="description" content="Contact page desc by create next app" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" href="/favicon.ico" />
+      <h2 className="text-3xl  ">Contact Page</h2>
+    </div>
+  );
 };
 
 export default contact;
